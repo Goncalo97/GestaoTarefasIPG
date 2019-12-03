b4094fab8af708e50903746154634cacf04d63e3
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2019-12-03 12:11:21 +0000

    Populate Servicos

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -11,6 +11,7 @@ namespace GestaoTarefasIPG.Models
         public static void Populate(GestaoTarefasIPGDbContext db)
         {
             PopulateEscolas(db);
+            PopulateServicos(db);
         }
 
         private static void PopulateEscolas(GestaoTarefasIPGDbContext db)
@@ -61,7 +62,16 @@ namespace GestaoTarefasIPG.Models
                 new Escola { Nome = "Escola Superior de Turismo e Hotelaria", Localizacao = "Seia", Descricao = "Onde se mistura" },
                 new Escola { Nome = "Escola Superior de Tecnologia e Gestão", Localizacao = "Guarda", Descricao = "Onde se aprende" }
             );
-
+            db.SaveChanges();
+        }
+        private static void PopulateServicos(GestaoTarefasIPGDbContext db)
+        {
+            if (db.Servico.Any()) return;
+            db.Servico.AddRange(
+                new Servico { Nome = "Calcular o número de alunos no curso de engenharia informática" },
+                new Servico { Nome = "Calcular o número de alunos que frequenta a aula de programação para a internet" },
+                new Servico { Nome = "Vigiar o teste de Análise Matemática" }
+            );
             db.SaveChanges();
         }
     }
