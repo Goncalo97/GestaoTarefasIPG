18757a697d79f7145e7ad629a7b35395f5615f19
RicardoMeloFig <48353967+RicardoMeloFig@users.noreply.github.com>
2020-01-08 16:00:38 +0000

    Migração para corrigir erro no cargos

---
+++ GestaoTarefasIPG/Migrations/20200108155937_cargosErro.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoTarefasIPG.Migrations
+{
+    public partial class cargosErro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "role",
+                table: "Cargo",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "role",
+                table: "Cargo");
+        }
+    }
+}
