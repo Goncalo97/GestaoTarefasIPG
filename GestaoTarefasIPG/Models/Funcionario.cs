171f1828b45298d9427695d58c62d7f7ea95e81e
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-01-14 11:39:19 +0000

    Criação do model "Funcionários"

---
+++ GestaoTarefasIPG/Models/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioID { get; set; }
+        [Required(ErrorMessage = "Por favor, insira um nome")]
+        [StringLength(248)]
+        [Display(Name = "Nome:")]
+        public string nome { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email:")]
+        public string email { get; set; }
+        [Required]
+        [Phone]
+        [Display(Name = "Telemovel:")]
+        public string telemovel { get; set; }
+    }
+}
