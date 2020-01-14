57985a7584f40fe3eaa8e3fdff75535e3fc4ce61
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-01-14 12:03:03 +0000

    Migração Funcionarios juntamente com o FuncionariosViewModel

---
+++ GestaoTarefasIPG/Migrations/20200114115928_Funcionarios.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoTarefasIPG.Migrations
+{
+    public partial class Funcionarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionario",
+                columns: table => new
+                {
+                    FuncionarioID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(maxLength: 248, nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    telemovel = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionario", x => x.FuncionarioID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionario");
+        }
+    }
+}
