0a53f475f1c3d798295ace49be276634ce2aad49
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2020-01-17 19:52:09 +0000

    Edit controller to match the functionality of the others

--- GestaoTarefasIPG/Controllers/DepartamentosController.cs
+++ GestaoTarefasIPG/Controllers/DepartamentosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GestaoTarefasIPG.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace GestaoTarefasIPG.Controllers
 {
@@ -13,19 +14,86 @@ namespace GestaoTarefasIPG.Controllers
     {
         private readonly GestaoTarefasIPGDbContext _context;
 
+        private const int NUMBER_OF_PRODUCTS_PER_PAGE = 10;
+        private const int NUMBER_OF_PAGES_BEFORE_AND_AFTER = 1;
+
         public DepartamentosController(GestaoTarefasIPGDbContext context)
         {
             _context = context;
         }
 
         // GET: Departamentos
-        public async Task<IActionResult> Index()
+        [Authorize(Roles = "admin")]
+        public IActionResult Index(int page = 1, string sortOrder = "Nome", string searchString = null, string searchOption = null)
         {
-            var gestaoTarefasIPGDbContext = _context.Departamento.Include(d => d.Escola);
-            return View(await gestaoTarefasIPGDbContext.ToListAsync());
+            decimal numberProducts = _context.Departamento.Count();
+            var _contextDepartamento = _context.Departamento.Include(d => d.Escola);
+            
+            DepartamentosViewModel vm = new DepartamentosViewModel
+            {
+                Departamentos = _contextDepartamento
+                .Take((int)numberProducts),
+                CurrentPage = page,
+                TotalPages = (int)Math.Ceiling(numberProducts / NUMBER_OF_PRODUCTS_PER_PAGE),
+                FirstPageShow = Math.Max(2, page - NUMBER_OF_PAGES_BEFORE_AND_AFTER),
+            };
+            var searchOptionList = new List<string>();
+
+            searchOptionList.Add("Nome");
+            searchOptionList.Add("Escola");
+
+            ViewBag.searchOption = new SelectList(searchOptionList);
+
+            if (!String.IsNullOrEmpty(searchString) && !String.IsNullOrEmpty(searchOption))
+            {
+                vm.CurrentSearchString = searchString;
+                switch (searchOption)
+                {
+                    case "Nome":
+                        vm.Departamentos = vm.Departamentos.Where(p => p.Nome.Contains(searchString, StringComparison.CurrentCultureIgnoreCase));
+                        vm.CurrentSearchOption = "Nome";
+                        break;
+                    case "Escola":
+                        vm.Departamentos = vm.Departamentos.Where(p => p.Escola.Nome.Contains(searchString, StringComparison.CurrentCultureIgnoreCase));
+                        vm.CurrentSearchOption = "Escola";
+                        break;
+                }
+            }
+            ViewBag.NomeSortParm = sortOrder == "Nome" ? "Nome_Desc" : "Nome";
+            ViewBag.Nome_EscolaSortParm = sortOrder == "Nome_Escola" ? "Nome_Escola_Desc" : "Nome_Escola";         
+            switch (sortOrder)
+            {
+                case "Nome_Desc":
+                    vm.Departamentos = vm.Departamentos.OrderByDescending(p => p.Nome);
+                    vm.CurrentSortOrder = "Nome_Desc";
+                    break;
+                case "Nome_Escola":
+                    vm.Departamentos = vm.Departamentos.OrderBy(p => p.Escola.Nome);
+                    vm.CurrentSortOrder = "Nome_Escola";
+                    break;
+                case "Nome_Escola_Desc":
+                    vm.Departamentos = vm.Departamentos.OrderByDescending(p => p.Escola.Nome);
+                    vm.CurrentSortOrder = "Nome_Escola_Desc";
+                    break;
+                default:
+                    vm.Departamentos = vm.Departamentos.OrderBy(p => p.Nome); // ascending by default
+                    vm.CurrentSortOrder = "Nome";
+                    break;
+
+            }
+            vm.TotalPages = (int)Math.Ceiling((decimal)vm.Departamentos.Count() / NUMBER_OF_PRODUCTS_PER_PAGE);
+            vm.Departamentos = vm.Departamentos.Skip((page - 1) * NUMBER_OF_PRODUCTS_PER_PAGE);
+            vm.Departamentos = vm.Departamentos.Take(NUMBER_OF_PRODUCTS_PER_PAGE);
+            vm.LastPageShow = Math.Min(vm.TotalPages, page + NUMBER_OF_PAGES_BEFORE_AND_AFTER);
+            vm.FirstPage = 1;
+            vm.LastPage = vm.TotalPages;
+            //return View(await gestaoTarefasIPGDbContext.ToListAsync());
+            return View(vm);
+
         }
 
         // GET: Departamentos/Details/5
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -45,6 +113,7 @@ namespace GestaoTarefasIPG.Controllers
         }
 
         // GET: Departamentos/Create
+        [Authorize(Roles = "admin")]
         public IActionResult Create()
         {
             ViewData["EscolaId"] = new SelectList(_context.Escola, "EscolaID", "Nome");
@@ -56,19 +125,34 @@ namespace GestaoTarefasIPG.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Create([Bind("DepartamentoId,Nome,EscolaId")] Departamento departamento)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(departamento);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                // se isto for null é porque não encontrou nenhum registo na tabela Escola com o mesmo Nome
+                if (_context.Escola.FirstOrDefault(m => m.Nome == departamento.Nome) == null)
+                {
+                    _context.Add(departamento);
+                    await _context.SaveChangesAsync();
+                    return View("Sucesso");
+                }
+                else
+                {
+                    ViewData["EscolaId"] = new SelectList(_context.Escola, "EscolaID", "Nome", departamento.EscolaId);
+                    // aparece a mensagem de erro em baixo do input do Nome
+                    ModelState.AddModelError("Nome", "Não é possível adicionar nomes repetidos.");
+                    return View(departamento);
+                }
             }
-            ViewData["EscolaId"] = new SelectList(_context.Escola, "EscolaID", "Nome", departamento.EscolaId);
-            return View(departamento);
+            else
+            {
+                return View("Erro");
+            }          
         }
 
         // GET: Departamentos/Edit/5
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -90,6 +174,7 @@ namespace GestaoTarefasIPG.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Edit(int id, [Bind("DepartamentoId,Nome,EscolaId")] Departamento departamento)
         {
             if (id != departamento.DepartamentoId)
@@ -101,8 +186,19 @@ namespace GestaoTarefasIPG.Controllers
             {
                 try
                 {
-                    _context.Update(departamento);
-                    await _context.SaveChangesAsync();
+                    // se isto for null é porque não encontrou nenhum registo na tabela Escola com o mesmo Nome
+                    if (_context.Escola.FirstOrDefault(m => m.Nome == departamento.Nome) == null)
+                    {
+                        _context.Update(departamento);
+                        await _context.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        ViewData["EscolaId"] = new SelectList(_context.Escola, "EscolaID", "Nome", departamento.EscolaId);
+                        // aparece a mensagem de erro em baixo do input do Nome
+                        ModelState.AddModelError("Nome", "Não é possível adicionar nomes repetidos.");
+                        return View(departamento);
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -115,13 +211,16 @@ namespace GestaoTarefasIPG.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return View("Sucesso");
             }
-            ViewData["EscolaId"] = new SelectList(_context.Escola, "EscolaID", "Nome", departamento.EscolaId);
-            return View(departamento);
+            else
+            {
+                return View("Erro");
+            }
         }
 
         // GET: Departamentos/Delete/5
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -143,12 +242,13 @@ namespace GestaoTarefasIPG.Controllers
         // POST: Departamentos/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var departamento = await _context.Departamento.FindAsync(id);
             _context.Departamento.Remove(departamento);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return View("Sucesso");
         }
 
         private bool DepartamentoExists(int id)
