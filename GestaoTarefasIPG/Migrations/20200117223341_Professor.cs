97cf608fc5079c95431f163596a54a2dbf3bfea2
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2020-01-17 22:58:35 +0000

    Add migration for new table and foreign key relation

---
+++ GestaoTarefasIPG/Migrations/20200117223341_Professor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoTarefasIPG.Migrations
+{
+    public partial class Professor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Departamento",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    ProfessorID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 60, nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Telemovel = table.Column<string>(nullable: false),
+                    Morada = table.Column<string>(maxLength: 80, nullable: false),
+                    DepartamentoID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.ProfessorID);
+                    table.ForeignKey(
+                        name: "FK_Professor_Departamento_DepartamentoID",
+                        column: x => x.DepartamentoID,
+                        principalTable: "Departamento",
+                        principalColumn: "DepartamentoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Professor_DepartamentoID",
+                table: "Professor",
+                column: "DepartamentoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professor");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Departamento",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+        }
+    }
+}
