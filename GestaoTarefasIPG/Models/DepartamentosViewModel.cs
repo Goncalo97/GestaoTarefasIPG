46e27ed34f3b225b6f59cfb14a57a98b5ff7d5be
Gonçalo <17440801+Goncalo97@users.noreply.github.com>
2020-01-17 19:30:33 +0000

    Add ViewModel to pass information between controller and html pages

---
+++ GestaoTarefasIPG/Models/DepartamentosViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class DepartamentosViewModel
+    {
+        public IEnumerable<Departamento> Departamentos { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int FirstPageShow { get; set; }
+        public int LastPageShow { get; set; }
+        public int FirstPage { get; set; }
+        public int LastPage { get; set; }
+        public string CurrentSortOrder { get; set; }
+        public string CurrentSearchOption { get; set; }
+        public string CurrentSearchString { get; set; }
+    }
+}
